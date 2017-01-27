1905224beb39de4b5eacedcc86fa90dbe924223c
Bo HOU <daily.bhou@gmail.com>
2017-01-27 04:09:08 +0100

    add configuration for letsencrypt

--- plugins/proxy-app/index.js
+++ plugins/proxy-app/index.js
@@ -32,6 +32,22 @@ module.exports = function (options, imports, register) {
   if (argv.so) {
     config.secureOnly = true;
   }
+  if (argv.domains) {
+    var domains = argv.domains.split(',');
+    config.domains = domains;
+  } else {
+    config.domains = [];
+  }
+  if (argv.email) {
+    config.email = argv.email;
+  } else {
+    config.email = '';
+  }
+  if (argv.prod) {
+    config.prod = true;
+  } else {
+    config.prod = false;
+  }
 
   // imports
   var logger = imports.logger.getLogger('Proxy');
@@ -48,7 +64,6 @@ module.exports = function (options, imports, register) {
     }));
   });
 
-
   // init upstream
   var upstreamDB = new UpstreamDB(argv.us);
   var adminApi = new AdminApi(upstreamDB);
@@ -58,54 +73,49 @@ module.exports = function (options, imports, register) {
     var webHandler = handlers.webHandler;
     var socketHandler = handlers.webSocketHandler;
 
-    function approveDomains(opts, certs, cb) {
-      if (certs) {
-        opts.domains = ['oobabyshop.com', 'oobaby.com.cn']; //certs.altnames;
+    if (config.secure) {
+      
+      logger.info('HTTPS enabled');
+      logger.info('letsencrypt config:');
+      logger.info('- approved domains:', config.domains);
+      logger.info('- email:', config.email);
+      logger.info('- for production:', config.prod);
+
+      function approveDomains(opts, certs, cb) {
+        if (certs) {
+          opts.domains = config.domains; //certs.altnames;
+        }
+        else {
+          opts.email = config.email; 
+          opts.agreeTos = true;
+        }
+
+        cb(null, { options: opts, certs: certs });
       }
-      else {
-        opts.email = 'plxpls.info@gmail.com';
-        opts.agreeTos = true;
-      }
 
-      cb(null, { options: opts, certs: certs });
-    }
-
-    var lex = require('letsencrypt-express').create({
-      server: 'staging'
-      , approveDomains: approveDomains
-    });
-
-    var httpServer = http.createServer(lex.middleware(require('redirect-https')())).listen(config.port);
-    httpServer.on('upgrade', socketHandler);
-    logger.info("HTTP: listening on port", config.port);
-
-    var httpsServer = https.createServer(lex.httpsOptions, lex.middleware(webHandler)).listen(config.securePort, function () {
-      console.log("Listening for ACME tls-sni-01 challenges and serve app on", config.securePort);
-    });
-    httpsServer.on('upgrade', socketHandler);
-
-    /*
-    var httpServer = http.createServer(le.middleware(webHandler)).listen(config.port);
-    httpServer.on('upgrade', socketHandler);
-    logger.info("HTTP: listening on port", config.port);
-
-    var httpsServer = https.createServer(le.middleware(webHandler)).listen(config.securePort);
-    httpsServer.on('upgrade', socketHandler);
-    logger.info("HTTPS: listening on port", config.securePort);
-    */
-
-    /*if (config.secure) {
-      var fs = require('fs');
-      var hskey = fs.readFileSync(argv.key ? path.join(global.home, argv.key) : path.join(global.home, '/key.pem'));
-      var hscert = fs.readFileSync(argv.key ? path.join(global.home, argv.cert) : path.join(global.home, '/cert.pem'));
-
-      var credentials = {
-        key: hskey,
-        cert: hscert
+      var lexOpt = {
+        server: config.prod ? 'https://acme-v01.api.letsencrypt.org/directory' : 'staging', 
+        approveDomains: approveDomains
       };
-      var httpsServer = https.createServer(credentials, webHandler).listen(config.securePort);
-      logger.info("HTTPS: listening on port", config.securePort);
-    }*/
+      logger.info('letsencrypt option - server:', lexOpt.server);
+      logger.info('letsencrypt option - approvedDomains:', config.domains);
+      var lex = require('letsencrypt-express').create(lexOpt);
+
+      var httpServer = http.createServer(lex.middleware(require('redirect-https')())).listen(config.port);
+      httpServer.on('upgrade', socketHandler);
+      logger.info("HTTP: listening on port", config.port);
+
+      var httpsServer = https.createServer(lex.httpsOptions, lex.middleware(webHandler)).listen(config.securePort, function () {
+        console.log("Listening for ACME tls-sni-01 challenges and serve app on", config.securePort);
+      });
+      httpsServer.on('upgrade', socketHandler);
+    } else {
+
+      logger.info('HTTPS disabled')
+      var httpServer = http.createServer(webHandler).listen(config.port);
+      httpServer.on('upgrade', socketHandler);
+      logger.info("HTTP: listening on port", config.port);
+    }
   });
 
   register(); // provides nothing
--- proxy.js
+++ proxy.js
@@ -8,7 +8,13 @@ runtime.start(__dirname, './architect-config', function() {
   console.log('--port http port, default: 80');
   console.log('--sp https port, default: 443');
   console.log('--es enable secure, default: false');
-  console.log('--so only secure, default: false');
-  console.log('--key certificate key file path, default: ./key.pem');
-  console.log('--cert certificate file path, default: ./cert.pem');
+
+  console.log('Letsencrypt Settings:');
+  console.log('--domains, list of approved domains, separated by comma (,)');
+  console.log('--email, email for configging letsencrypt');
+  console.log('--prod, if using prod letsencrypt server, default: false');
+  
+  //console.log('--so only secure, default: false');
+  //console.log('--key certificate key file path, default: ./key.pem');
+  //console.log('--cert certificate file path, default: ./cert.pem');
 });
